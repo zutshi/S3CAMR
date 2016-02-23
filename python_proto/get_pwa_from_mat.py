2cf348d23d9ec9eaa688938bb12779cac9ab8cd1
zutshi <aaditya.zutshi@gmail.com>
2016-02-22 20:42:56 -0700

    removed fixed paths, now accepts args

--- python_proto/get_pwa_from_mat.py
+++ python_proto/get_pwa_from_mat.py
@@ -1,18 +1,20 @@
+import argparse
 import scipy.io as sio
+
 import scipy as sp
 import pickle
 
 import pwa
 import fileOps as fops
 
-sp.set_printoptions(suppress=True, precision=2)
+#sp.set_printoptions(suppress=True, precision=2)
 
-MODEL_MAT = '../examples/vdp/vdp_model_data_x_1e6.mat'
-PICKLE_FNAME = '../examples/vdp/pickled_model'
+#MODEL_MAT = '../examples/vdp/vdp_model_data_x_1e6.mat'
+#PICKLE_FNAME = '../examples/vdp/pickled_model'
 
 
-def populate():
-    model_data = sio.loadmat(MODEL_MAT)
+def populate(model_mat):
+    model_data = sio.loadmat(model_mat)
     # flattened model is stored as [(P0,M0),...(Pi, Mi),...(Pn,Mn)]
     fm = model_data['fm']
     pwa_rep = pwa.PWA()
@@ -30,18 +32,23 @@ def populate():
     return pwa_rep
 
 
-def test_pickling(pwa_rep):
+def test_pickling(pwa_rep, pickle_fname):
     pickled_pwa_w = pickle.dumps(pwa_rep, pickle.HIGHEST_PROTOCOL)
-    fops.write_data(PICKLE_FNAME, pickled_pwa_w)
-    pickled_pwa_r = fops.get_data(PICKLE_FNAME)
+    fops.write_data(pickle_fname, pickled_pwa_w)
+    pickled_pwa_r = fops.get_data(pickle_fname)
     return pickle.loads(pickled_pwa_r)
 
 
-def main():
-    pwa_rep = populate()
+def main(model_mat, pickle_fname):
+    pwa_rep = populate(model_mat)
     #print pwa_rep
-    pwa_rep = test_pickling(pwa_rep)
+    pwa_rep = test_pickling(pwa_rep, pickle_fname)
     print pwa_rep
 
 if __name__ == '__main__':
-    main()
+    usage = '%(prog)s <filename>'
+    parser = argparse.ArgumentParser(description='demo pwa', usage=usage)
+    parser.add_argument('mat_file', default=None, type=str)
+    parser.add_argument('-o', default=None, metavar='pickle', type=str, required=True)
+    args = parser.parse_args()
+    main(args.mat_file, args.o)
