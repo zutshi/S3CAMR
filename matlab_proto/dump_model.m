5bb09e9fe82adf6a1be2b10b700bbc34c5e88d2c
zutshi <aditya.zutshi@colorado.edu>
2016-02-24 10:04:48 -0700

    added a function to handle dumping of xt models

--- matlab_proto/dump_model.m
+++ matlab_proto/dump_model.m
@@ -1,4 +1,9 @@
+function dump_model()
+dump_xt();
+%dump_x();
+end
 
+function dump_x()
 % x0 = [-0.4 0.4; -0.4 0.4];
 x0 = [0.3 0.4; -0.4 -0.3];
 prop = [-1, -0.7; -6.5 -5.6];
@@ -21,3 +26,26 @@ GA = GridAbstraction(eps);
 RA = RelAbs(model, GA, Range, model_delta_t);
 fm = RA.get_flattened_model();
 save('flat_model', 'fm')
+end
+
+
+
+function dump_xt()
+
+FILE = './vdp_xt_1e6_data.mat';
+DATA = load(FILE);
+
+X = DATA.Y_summary(:, 1:3);
+Y = DATA.Y_summary(:, 3:4);
+% Create abstraction
+eps = [1,1,0.5];
+%Range = [min(X)', max(X)'];
+Range = [-2, 2; -8 8; 0 1];
+fprintf('getting model...\n')
+model = Model(Range, eps, Y, X);
+
+GA = GridAbstraction(eps);
+RA = RelAbs(model, GA, Range, 0);
+fm = RA.get_flattened_model();
+save([FILE 'flat_model_'], 'fm')
+end
