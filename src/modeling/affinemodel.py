08716c0e4abf933c5299650fab280e60dfbbce36
zutshi <aaditya.zutshi@gmail.com>
2016-03-20 02:07:41 -0600

    fixed cell splitting to get models of correct tolerance. Is useless due to obscenely inaccurate underlying paths discovered by scatter-sim

--- src/modeling/affinemodel.py
+++ src/modeling/affinemodel.py
@@ -7,8 +7,8 @@ class AffineModel(object):
         clf_ = skl_lm.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=True)
         self.clf_ = clf_
         clf_.fit(x, y)
-        if __debug__:
-            print clf_.coef_, clf_.intercept_
+        #if __debug__:
+        #    print clf_.coef_, clf_.intercept_
 
     @property
     def clf(self):
@@ -25,7 +25,7 @@ class AffineModel(object):
     def model_error(self, X, Y):
         '''computes relative error along each dimension'''
         Y_ = self.predict(X)
-        if __debug__:
-            print 'score: ', self.clf.score(X, Y)
+        #if __debug__:
+            #print 'score: ', self.clf.score(X, Y)
             #print abs((Y - Y_)/Y)*100
         return np.max((abs((Y - Y_)/Y))*100, axis=0)
