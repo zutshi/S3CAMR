0a143f259601b80a65521813e250329c4a7a4bbd
zutshi <aaditya.zutshi@gmail.com>
2016-03-29 21:26:47 -0600

    minor fixes added first_sat function

--- src/scam_core/properties.py
+++ src/scam_core/properties.py
@@ -12,11 +12,26 @@ class PropertyChecker():
         T : Time Vector of the trace
         Y : Array of state vectors of the trace
         """
-        # XXX: any() seems to work although np.any will be the riight
+        # XXX: any() seems to work although np.any will be the right
         # function. Looks like any() just works on the array as an
         # iterator.
         return any(self.final_cons.contains(Y))
         #return np.any(self.final_cons.contains(Y))
+
+    def first_sat_value_or_end(self, T, Y):
+        """first_sat_value_or_end
+        Gets the first satisfying state else returns the end of the
+        trace.
+        Parameters
+        ----------
+        T : Time Vector of the trace
+        Y : Array of state vectors of the trace
+        """
+        sat = self.final_cons.contains(Y)
+        if any(sat):
+            return (T[sat][0], Y[sat][0]), True
+        else:
+            return (T[-1], Y[-1]), False
 
     def check(self, t, y):
         """check
