33eaab2c1c5cd11b1f3d69acf4f926861e0e00b1
zutshi <aaditya.zutshi@gmail.com>
2016-09-27 14:47:36 -0600

    moved plotting to settings

--- src/settings.py
+++ src/settings.py
@@ -4,3 +4,11 @@ debug_plot = False
 
 # CE hack is ON
 CE = True
+
+
+def plt_show():
+    from matplotlib import pyplot as plt
+    if debug_plot:
+        plt.show()
+    else:
+        plt.close()
