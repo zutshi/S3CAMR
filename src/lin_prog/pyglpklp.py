81563ce4a2525cdd6b33afce1fbbe29dd8b6fc62
zutshi <aaditya.zutshi@gmail.com>
2016-10-06 14:10:30 -0600

    pyglpk support

---
+++ src/lin_prog/pyglpklp.py
@@ -0,0 +1,50 @@
+import collections
+
+import glpk
+
+# Tuple contains lp results. It follows the scipy convention
+OPTRES = collections.namedtuple('optres', ('fun', 'status', 'success'))
+
+
+# TODO: make it handle multiple objectives?
+# Specifially if just changing an obj offers a warm restart? It will
+# also eschew the time required to re-create the LP. WIll need to
+# research if such an optimization is legal and does it actually save
+# enough time?
+def linprog(obj, A, b):
+    """linprog
+
+    Parameters
+    ----------
+    c : obj as a list
+    A : as an numpy array
+    b : as a numpy array
+
+    Returns
+    -------
+    s
+
+    Notes
+    ------
+    """
+    lp = glpk.LPX()
+    lp.name = 'min x0'
+    lp.obj.maximize = False
+
+    lp.rows.add(len(b))
+
+    for idx, r in enumerate(lp.rows):
+        r.name = 'r{}'.format(idx)
+        r.bounds = None, b[idx]
+
+    lp.cols.add(A.shape[1])
+
+    for idx, c in enumerate(lp.cols):
+        c.name = 'x{}'.format(idx)
+        c.bounds = None, None
+
+    lp.obj[:] = obj.tolist()
+
+    lp.matrix = A.flatten()
+    lp.simplex()
+    return OPTRES(lp.obj.value, lp.status, lp.status == 'opt')
