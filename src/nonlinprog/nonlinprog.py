ad6744fb8b672de3444aa49d2d789c67e67852f1
zutshi <aaditya.zutshi@gmail.com>
2017-06-09 16:30:04 -0400

    cleanup

--- src/nonlinprog/nonlinprog.py
+++ src/nonlinprog/nonlinprog.py
@@ -5,24 +5,13 @@ from __future__ import unicode_literals
 
 
 def factory(solver_str):
-    if solver_str == 'glpk':
-        import linprog.pyglpklp as pyglpklp
-        #res = [pyglpklp.linprog(obj, A_ub, b_ub) for obj in directions_ext]
-        #lp_fun = ft.partial(pyglpklp.linprog, A_ub=A_ub, b_ub=b_ub)
-        lp_fun = pyglpklp.linprog
-
-    elif solver_str == 'gurobi':
-        import linprog.pygurobi as pygurobi
-        #res = [pygurobi.linprog(obj, A_ub, b_ub) for obj in directions_ext]
-        #lp_fun = ft.partial(pygurobi.linprog, A_ub=A_ub, b_ub=b_ub)
-        lp_fun = pygurobi.linprog
+    if solver_str == 'z3':
+        from nonlinprog.z3opt import nlinprog
 
     elif solver_str == 'scipy':
-        import linprog.scipyopt as scipyopt
-        #lp_fun = ft.partial(scipyopt.linprog, A_ub=A_ub, b_ub=b_ub)
-        lp_fun = scipyopt.linprog
+        from nonlinprog.scipyopt import nlinprog
 
     else:
         raise NotImplementedError('solver selected: {}'.format(solver_str))
 
-    return lp_fun
+    return nlinprog
