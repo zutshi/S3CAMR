653761807bc570f47a951e7857c3f1ebd9badce5
zutshi <aaditya.zutshi@gmail.com>
2017-06-20 22:25:03 -0400

    core/modelrefine.py
    added opt in dreal thru ipopt...temporay fix

--- src/nonlinprog/dreal.py
+++ src/nonlinprog/dreal.py
@@ -19,7 +19,7 @@ from globalopts import opts as gopts
 import nonlinprog.spec as spec
 
 DREAL = '/home/zutshi/software/dreal3/build/dReal'
-DELTA_SAT_PREC = '0.001'
+DELTA_SAT_PREC = '0.01'
 FNAME = 'tmp.smt2'
 
 
@@ -37,9 +37,33 @@ def nlinprog(obj, cons, Vars):
     Notes
     ------
     """
-    # ignoring objective, will check only feasibilit for now
-    if obj != 0:
-        raise NotImplementedError
+    # feasibility check
+    if obj == 0:
+        res = check_feasibility(obj, cons, Vars)
+        if res.success and res_is_valid(Vars, cons, res):
+            return res
+        else:
+            return spec.OPTRES(0, None, 'OK', False)
+    else:
+        import nonlinprog.ipopt as ipopt
+        res = check_feasibility(obj, cons, Vars)
+        res_nlopt = ipopt.nlinprog(obj, cons, Vars, x0=res.x)
+        if not res_nlopt.success:
+            return res
+        else:
+            return res_nlopt
+
+
+def res_is_valid(Vars, cons, res):
+    varval_map = {v: x for v, x in zip(Vars, res.x)}
+    for c in cons:
+        if c.subs(varval_map) > 0:
+            return False
+    return True
+
+
+
+def check_feasibility(obj, cons, Vars):
 
     var_str_to_idx = {str(v): idx for idx, v in enumerate(Vars)}
 
@@ -87,11 +111,11 @@ def parse_dreal_res(var_str_to_idx, output):
         assert(op[0] == 'Solution:')
         assert(op[-1][:23] == 'delta-sat with delta = ')
 
-        # we select lower bounds for no specific reasons
+        # we select avg. of the lower bounds and upper bounds for no specific reasons
         x = np.zeros(nVars)
         for a in op[1:-1]:
             v, lb, ub = parse_assignment(a)
-            x[var_str_to_idx[v]] = lb
+            x[var_str_to_idx[v]] = (float(lb)+float(ub))/2
         return True, x
 
 
